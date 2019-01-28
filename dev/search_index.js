d0661bf33594405ac5dac4a0d2f22bc636bf37b9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-28 04:42:09 +0000

    build based on c25db08

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Mathy.@$",
     "category": "macro",
-    "text": "@$      op dot_expr\n@$ init op dot_expr\n@$      op { dot_expr | filter_expr }\n@$ init op { dot_expr | filter_expr }\n\nExamples\n\njulia> using Mathy\n\njulia> @$ + (1:10) .^ 2\n385\n\njulia> ans == sum((1:10) .^ 2)\ntrue\n\njulia> @$ 0.0 + (1:10) .^ 2\n385.0\n\njulia> @$ 1000 + (1:10) .^ 2\n1385\n\njulia> @$ + { (1:10) .^ 2 | isodd(_) }\n165\n\njulia> ans == sum(x for x in (1:10) .^ 2 if isodd(x))\ntrue\n\njulia> @$ + { (1:10) .^ 2 | (_ + 2) % 3 == 0 }\n259\n\njulia> ans == sum(x for x in (1:10) .^ 2 if (x + 2) % 3 == 0)\ntrue\n\njulia> ∑ = +\n       ∏ = *;\n\njulia> @$ ∑ { (1:10) .^ 2 | (_ + 2) % 3 == 0 }\n259\n\njulia> @$ ∏ { (1:10) .^ 2 | (_ + 2) % 3 == 0 }\n501760000\n\n\n\n\n\n"
+    "text": "@$      op dot_expr\n@$ init op dot_expr\n@$      op { dot_expr | filter_expr }\n@$ init op { dot_expr | filter_expr }\n\nExamples\n\njulia> using Mathy\n\njulia> @$ + (1:10) .^ 2\n385\n\njulia> ans == sum((1:10) .^ 2)\ntrue\n\njulia> @$ 0.0 + (1:10) .^ 2\n385.0\n\njulia> @$ 1000 + (1:10) .^ 2\n1385\n\njulia> @$ + { (1:10) .^ 2 | isodd(_) }\n165\n\njulia> ans == sum(x for x in (1:10) .^ 2 if isodd(x))\ntrue\n\njulia> @$ + { (1:10) .^ 2 | (_ + 2) % 3 == 0 }\n259\n\njulia> ans == sum(x for x in (1:10) .^ 2 if (x + 2) % 3 == 0)\ntrue\n\njulia> ∑ = +\n       ∏ = *;\n\njulia> @$ ∑ { (1:10) .^ 2 | (_ + 2) % 3 == 0 }\n259\n\njulia> @$ ∏ { (1:10) .^ 2 | (_ + 2) % 3 == 0 }\n501760000\n\nCurly braces can be nested (although it is not optimized yet as of Transducers 0.2.1):\n\njulia> @$ + { 1:10 | isodd(_) } .^ 2\n165\n\njulia> @$ 0 + { 2 .* ({ 1:10 | isodd(_) } .^ 2 .+ 1) .- 1 | _ % 3 == 0 }\n153\n\njulia> ans == sum(filter(x -> x % 3 == 0, 2 .* (filter(isodd, 1:10) .^ 2 .+ 1) .- 1))\ntrue\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Mathy.@eduction-Tuple{Any}",
+    "page": "Home",
+    "title": "Mathy.@eduction",
+    "category": "macro",
+    "text": "@eduction dot_expr\n@eduction { dot_expr | filter_expr }\n\nLike @$ but without op.\n\nExamples\n\njulia> using Mathy\n\njulia> collect(@eduction { (1:10) .^ 2 | isodd(_) })\n5-element Array{Int64,1}:\n  1\n  9\n 25\n 49\n 81\n\n\n\n\n\n"
 },
 
 {
@@ -21,7 +29,39 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Mathy.jl",
     "category": "section",
-    "text": "Modules = [Mathy]"
+    "text": "Pages = [\"index.md\"]Modules = [Mathy]\nPrivate = false"
+},
+
+{
+    "location": "internals/#",
+    "page": "Internals",
+    "title": "Internals",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "internals/#Mathy.isdotbracecall-Tuple{Any}",
+    "page": "Internals",
+    "title": "Mathy.isdotbracecall",
+    "category": "method",
+    "text": "isdotbracecall(x)\n\nExamples\n\njulia> using Mathy: isdotbracecall\n\njulia> isdotbracecall(:(f.{x}))\ntrue\n\njulia> isdotbracecall(:(f.(x)))\nfalse\n\njulia> isdotbracecall(:(f{x}))\nfalse\n\n\n\n\n\n"
+},
+
+{
+    "location": "internals/#Mathy.isdotcall-Tuple{Any}",
+    "page": "Internals",
+    "title": "Mathy.isdotcall",
+    "category": "method",
+    "text": "isdotcall(x)\n\nExamples\n\njulia> using Mathy: isdotcall\n\njulia> isdotcall(:(f.(x)))\ntrue\n\njulia> isdotcall(:(f(x)))\nfalse\n\njulia> isdotcall(:(x .+ y))  # handled separately\nfalse\n\n\n\n\n\n"
+},
+
+{
+    "location": "internals/#Internals-1",
+    "page": "Internals",
+    "title": "Internals",
+    "category": "section",
+    "text": "Modules = [Mathy]\nPublic = false"
 },
 
 ]}
